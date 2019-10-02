606b5c7465e1e3dfd8e41fbacf71f129bc3db337
Matthew Deatherage <mdeatherage1992@gmail.com>
2019-10-02 11:01:19 -0400

    prettying changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
 
 def home
+  if user_signed_in?
   @new_org = Organization.new
   @orgs = Organization.all
+else
+  redirect_to new_user_session_path
+  end
 end
 
 end
